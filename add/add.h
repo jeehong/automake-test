6804ff5dbfcdf55006c83a5d65081182983be69d
jeehong <jeehong2015@gmail.com>
2016-06-15 11:40:02 +0800

    Create demo code of automake-test

---
+++ add/add.h
@@ -0,0 +1,8 @@
+/* add.h */
+#ifndef __ADD_H__
+#define __ADD_H__
+
+/*整型加和浮点型加的声明*/
+int add_int(int a, int b);
+
+#endif /*__ADD_H__*/
---
+++ sub/sub.h
@@ -0,0 +1,8 @@
+/* sub.h */
+#ifndef __SUB_H__
+#define __SUB_H__
+
+/*整型减和浮点型减的声明*/
+int sub_int(int a, int b);
+
+#endif /*__SUB_H__*/
